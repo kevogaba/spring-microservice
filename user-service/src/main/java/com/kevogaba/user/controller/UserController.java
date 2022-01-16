7c667dc39f56b8076668249432c7ffaa33c05a72
Kevin Ogaba <kevogaba@gmail.com>
2022-01-16 12:08:23 +0300

    user service

---
+++ user-service/src/main/java/com/kevogaba/user/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.kevogaba.user.controller;
+
+import com.kevogaba.user.VO.ResponseTemplateVO;
+import com.kevogaba.user.entity.User;
+import com.kevogaba.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user) {
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId) {
+        log.info("Inside getUserWithDepartment of UserController");
+        return userService.getUserWithDepartment(userId);
+    }
+}
