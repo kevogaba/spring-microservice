582f6af7140bcc0e91ae3642208821aa2a9ed6c1
Kevin Ogaba <kevogaba@gmail.com>
2022-01-23 14:22:07 +0300

    Dockerised application

--- cloud-gateway/src/main/java/com/kevogaba/cloud/gateway/FallBackMethodController.java
+++ cloud-gateway/src/main/java/com/kevogaba/cloud/gateway/FallBackMethodController.java
@@ -1,5 +1,6 @@
 package com.kevogaba.cloud.gateway;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class FallBackMethodController {
 
     @GetMapping("/userServiceFallBack")
+    @HystrixCommand(fallbackMethod = "userServiceFallBackMethod")
     public String userServiceFallBackMethod() {
         return "User service is taking longer than expected." +
                 "Please try again later.";
     }
 
     @GetMapping("/departmentServiceFallBack")
+    @HystrixCommand(fallbackMethod = "departmentServiceFallBackMethod")
     public String departmentServiceFallBackMethod() {
         return "Department service is taking longer than expected." +
                 "Please try again later.";
